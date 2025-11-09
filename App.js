64224d37de3789836834a40429a5737a529ead52
krishnamoorthyBFX <krishnamoorthy.k@bfxinfotech.com>
2025-11-10 00:24:13 +0530

    feat: all about parcel

--- App.js
+++ App.js
@@ -1,16 +1,23 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
 const parent = React.createElement(
   "div",
   { id: "parent" },
   React.createElement("div", { id: "child" }, [
     React.createElement(
       "h1",
-      { id: "header" },
+      { id: "header", key: "h1" },
       "This is the header in nested element"
     ),
-    React.createElement("h2", { id: "header" }, "This is second header"),
+    React.createElement(
+      "h2",
+      { id: "header", key: "h2" },
+      "This is second header"
+    ),
   ])
 );
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
 
 root.render(parent);
---
+++ README.md
@@ -0,0 +1,19 @@
+#parcel
+
+- Dev Build.
+- Local Server.
+- HMR = Hot Module Replacement.
+- File Watching Algorithm written in c++.
+- Caching faster Builds.
+- Image optimization.
+- Production build file minification.
+- Bundling the file.
+- Compress the files.
+- Consistent hashing.
+- Read code Snippet.
+- Differential Bundling - support older browsers too.
+- Diagnostics
+- Error Handling.
+- Helps to host our application on HTTPS.
+- Tree shaking - remove unused code
+- Different dev and production bundle.
